a7ced766e4ca2249a51191ca465cbe1649149552
Tetsu Yamakawa <68310167+tecchu11@users.noreply.github.com>
2024-08-02 23:34:05 +0900

    add ci and impl test (#3)

---
+++ internal/client_test.go
@@ -0,0 +1,41 @@
+package internal_test
+
+import (
+	"encoding/json"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/tecchu11/lambda-invoker-go/internal"
+)
+
+func TestIntegration(t *testing.T) {
+	type Event struct {
+		Msg string
+	}
+	handler := func(event Event) (string, error) {
+		return event.Msg, nil
+	}
+	go func() {
+		t.Setenv("_LAMBDA_SERVER_PORT", "9000")
+		lambda.Start(handler)
+	}()
+	time.Sleep(100 * time.Millisecond)
+
+	client, err := internal.New(9000)
+	if err != nil {
+		t.Fatal(err)
+	}
+	buf, err := json.Marshal(Event{Msg: "ok"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	res, err := client.Do(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(res) != `"ok"` {
+		t.Fatal(string(res))
+	}
+
+}
